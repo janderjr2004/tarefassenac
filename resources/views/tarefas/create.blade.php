b87751455c82d53cc1a8889ffed7802995e3ea38
Jander De Castro Cunha Junior <860966@senacminas.edu.br>
2023-06-05 21:34:57 -0300

    Tarefas completo

---
+++ resources/views/tarefas/create.blade.php
@@ -0,0 +1,74 @@
+@extends('adminlte::page')
+@section('content')
+@if($errors->any())
+
+<div class="alert alert-danger">
+
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+
+</div>
+
+@endif
+
+<div class="card">
+    <div class="card-header">Adicionar Tarefa</div>
+    <div class="card-body">
+        <form method="post" action="{{ route('tarefas.store') }}" enctype="multipart/form-data">
+
+            @csrf
+
+            <div class="row mb-3">
+                <label class="col-sm-2 col-label-form">Nome da Tarefa</label>
+                <div class="col-sm-10">
+                    <input type="text" name="tarefa_titulo" class="form-control" />
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-2 col-label-form">Descrição da Tarefa</label>
+                <div class="col-sm-10">
+                    <input type="text" name="tarefa_descricao" class="form-control" />
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Status</label>
+                <div class="col-sm-10">
+                    <select name="tarefa_status" class="form-control">
+                        <option value="Concluido">Concluído</option>
+                        <option value="Nao Concluido">Não Concluído</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Data de Conclusão</label>
+                <div class="col-sm-10">
+                    <input type="date" name="tarefa_data" class="form-control" />
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Prioridade</label>
+                <div class="col-sm-10">
+                    <select name="tarefa_prioridade" class="form-control">
+                        <option value="Alta">Alta</option>
+                        <option value="Media">Média</option>
+                        <option value="Baixa">Baixa</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <input type="submit" class="btn btn-primary" value="Adicionar" />
+            </div>
+
+        </form>
+    </div>
+</div>
+
+@endsection('content')
---
+++ resources/views/tarefas/edit.blade.php
@@ -0,0 +1,65 @@
+@extends('adminlte::page')
+@section('content')
+
+ <div class="card">
+    <div class="card-header">Editar Tarefa</div>
+    <div class="card-body">
+        <form method="post" action="{{ route('tarefas.update', $tarefa->id) }}" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+
+            <div class="row mb-3">
+                <label class="col-sm-2 col-label-form">Nome da Tarefa</label>
+                <div class="col-sm-10">
+                    <input type="text" name="tarefa_titulo" class="form-control" value="{{ $tarefa->tarefa_titulo }}" />
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-2 col-label-form">Descrição da Tarefa</label>
+                <div class="col-sm-10">
+                    <input type="text" name="tarefa_descricao" class="form-control" value="{{ $tarefa->tarefa_descricao }}" />
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Status</label>
+                <div class="col-sm-10">
+                    <select name="tarefa_status" class="form-control">
+                        <option value="Concluido">Concluído</option>
+                        <option value="Nao Concluido">Não Concluído</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Data de Conclusão</label>
+                <div class="col-sm-10">
+                    <input type="date" name="tarefa_data" class="form-control" value="{{ $tarefa->tarefa_data }}" />
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <label class="col-sm-2 col-label-form">Prioridade</label>
+                <div class="col-sm-10">
+                    <select name="tarefa_prioridade" class="form-control">
+                        <option value="Alta">Alta</option>
+                        <option value="Media">Média</option>
+                        <option value="Baixa">Baixa</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="text-center">
+                <input type="hidden" name="hidden_id" value="{{ $tarefa->id }}" />
+                <input type="submit" class="btn btn-primary" value="Editar" />
+            </div>
+
+        </form>
+    </div>
+</div>
+<script>
+    document.getElementsByName('tarefa_prioridade')[0].value = "{{ $tarefa->tarefa_prioridade }}";
+</script>
+
+ @endsection('content')
